61e36f2352e3ac6d74ed892e9207e5a84cbb8338
colin <colin@colin.com>
2019-02-15 14:20:08 +0800

    IU-2018.2.6 <lenovo@LAPTOP-FMKIDGSR Create ide.general.xml, git.xml, print.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, keymap.xml, colors.scheme.xml, databaseDrivers.xml, mavenVersion.xml, templates.xml, AnnotationType.java, javarebel.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Eclipse copy.xml, macros.xml, vcs.xml, Solarized Light (Alternate).icls, mybatis.xml, editor.xml, debugger.xml, MyGroup.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Author: colin
+ * @Date: ${DATE} ${HOUR}:${MINUTE}
+ * @Description: $end
+ * @Version: V1.0
+ */
+public class ${NAME} {
+    
+}
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Author: colin
+ * @Date: ${DATE} ${HOUR}:${MINUTE}
+ * @Description: $end
+ * @Version: V1.0
+ */
+public interface ${NAME} {
+}
